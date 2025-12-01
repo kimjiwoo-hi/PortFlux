3ce0520b300d3bc6aae643e3f8d236da6f903e04
FillDDak <cgr456@naver.com>
2025-12-01 15:59:37 +0900

    Cors 설정 수정
    - 문제 발생하여 @NonNull 추가

--- backend/src/main/java/com/portflux/backend/config/CorsConfig.java
+++ backend/src/main/java/com/portflux/backend/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.portflux.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOrigins("http://localhost:3000", "http://localhost:5173") // 프론트엔드 개발 서버 주소 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
