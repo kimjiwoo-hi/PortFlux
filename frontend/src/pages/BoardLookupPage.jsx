3afa676568f98d00b545f929f89b55a2a707d063
kimjiwoo-hi <rlawldn1015@gmail.com>
2025-12-03 15:05:58 +0900

    s	modified:   frontend/src/pages/BoardLookupPage.jsx 보드룩업페이지.jsx 기본 코드 작성 - 기본작성 시도 :w
    :w

---
+++ frontend/src/pages/BoardLookupPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./BoardLookupPage.css";
+
+
+function BoardLookupPage() {
+    return (
+        <div>
+        <h1>Board Lookup Page</h1>
+        <BoardLookup />
+        </div>
+    );
+    }
+
+export default BoardLookupPage;
