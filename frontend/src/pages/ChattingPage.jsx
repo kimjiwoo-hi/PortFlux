4a9bc2dc41574c6433fbe81be17e0a119e3ce30b
SangHunKim <kim199603330@gmail.com>
2025-12-04 18:13:14 +0900

    test-sang

---
+++ frontend/src/pages/ChattingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ChattingPage.css";
+
+const ChattingPage = () => {
+  return (
+    <div className="chatting-page"/> 
+    );
+};
+
+export default ChattingPage;
