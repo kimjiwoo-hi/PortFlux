5b0d2c857595d454eaf17d19175144439ae090ec
kimjiwoo-hi <rlawldn1015@gmail.com>
2025-12-05 16:48:43 +0900

    검색바 스타일 추가 돋보기 추가 search.png추가

--- frontend/src/pages/BoardLookupPage.jsx
+++ frontend/src/pages/BoardLookupPage.jsx
@@ -1,11 +1,20 @@
 import "./BoardLookupPage.css";
+import SearchIcon from '../assets/search.png';
 
 function BoardLookupPage() {
   return (
     <div className="tag-box">
-      <div className="tag-search-bar">Search...</div>
+      <div className="tag-search-area">
+        <div className="tag-search-bar">Search...</div>
+        <div className="tag-search-circle">
+          <img src={SearchIcon} alt="Search Icon" className="search-icon" />
+        </div>
+      </div>
     </div>
-  );
+    
+
+);
+  
 }
 
 export default BoardLookupPage;
