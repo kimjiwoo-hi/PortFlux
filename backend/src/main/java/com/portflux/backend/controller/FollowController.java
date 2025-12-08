5726ad2ecfcb5aae287a74d91772701996d93b16
SangHunKim <kim199603330@gmail.com>
2025-12-08 14:44:52 +0900

    1208text

--- backend/src/main/java/com/portflux/backend/controller/FollowController.java
+++ backend/src/main/java/com/portflux/backend/controller/FollowController.java
@@ -1,5 +1,8 @@
 package com.portflux.backend.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class FollowController {
     
 }
--- backend/src/main/java/com/portflux/backend/mapper/FollowMapper.java
+++ backend/src/main/java/com/portflux/backend/mapper/FollowMapper.java
@@ -1,26 +1,41 @@
 package com.portflux.backend.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+
+import com.portflux.backend.beans.FollowBean;
 
 @Mapper
 public interface FollowMapper {
     
     //팔로워 추가
-    int insertFollow(@Param("followerId") Long followerId,
-                     @Param("followeeId") Long followeeId);
+    @SelectKey(statement = "SELECT FOLLOWS_SEQ.NEXTVAL FROM dual",
+               keyProperty = "follow_id",
+               before = true,
+               resultType = Long.class)
+    @Insert("INSERT INTO FOLLOWS (follow_id, follower_id, following_id, created_at) " +
+            "VALUES (#{follow_id}, #{follower_id}, #{following_id}, SYSDATE)")
+            void insertFollow(FollowBean followBean);
 
     //팔로워 삭제
-    int deleteFollow(@Param("followerId") Long followerId,
-                     @Param("followeeId") Long followeeId);
-    
+    @Delete("DELETE FROM FOLLOWS WHERE follower_id = #{followerId} AND following_id = #{followingId}")
+    void deleteFollow(FollowBean followBean);
+
     //팔로우 존재 여부 확인
-    int existsFollow(@Param("followerId") Long followerId,
-                     @Param("followeeId") Long followeeId);
+    @Select("SELECT COUNT(*) FROM FOLLOWS WHERE follower_id = #{followerId} AND following_id = #{followingId}")
+    int existsFollow(FollowBean followBean);
+
     //팔로워 수 조회
-    int countFollowers(@Param("userId") Long userId);
+    @Select("Select following_id From Follows where follower_id = #{followerId}")
+    List<Long> selectFollowerIds(Long followerId);
 
     //팔로잉 수 조회
-    int countFollowing(@Param("userId") Long userId);
-
+    @Select("Select follower_id From Follows where following_id = #{followingId}")
+    List<Long> selectFollowingIds(Long followingId);
 }
