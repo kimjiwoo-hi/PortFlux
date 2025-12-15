427f8991773a92bc5d6d72331620dfc63ec1d0c3
donghoo80 <rlaehdgn80@gmail.com>
2025-12-15 12:03:16 +0900

    1. 페이지 이동시 스크롤을 맨 위로 올려주는 컴포넌트    Header, NoFooterLayout에 추가 2. 백엔드 이메일 설정 수정 portflux7@gmail.com 이메일 생성 3. 로그인시 로그인/회원가입 버튼 Header에서 숨김 4. 로그인 유지 기능 만듬

--- frontend/src/layouts/NoFooterLayout.jsx
+++ frontend/src/layouts/NoFooterLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import ScrollToTop from "../components/ScrollToTop"; 
 import "./NoFooterLayout.css";
 
 const NoFooterLayout = () => {
   return (
     <div className="main-layout">
+      <ScrollToTop /> 
       <Header />
       <main className="main-content">
         <Outlet />
