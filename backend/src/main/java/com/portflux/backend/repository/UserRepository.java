91ac7fe11a9aa349c24af35d81cc19d9f4943e61
donghoo80 <rlaehdgn80@gmail.com>
2025-12-15 18:14:21 +0900

    회원가입 닉네임 유효성 오류, 기업 회원가입 오류 수정

--- backend/src/main/java/com/portflux/backend/repository/UserRepository.java
+++ backend/src/main/java/com/portflux/backend/repository/UserRepository.java
@@ -21,4 +21,7 @@ public interface UserRepository extends JpaRepository<UserBean, Long> {
 
     // 5. ★ 이름과 이메일로 유저 조회 (아이디 찾기 기능용)
     UserBean findByUserNameAndUserEmail(String userName, String userEmail);
+
+    // 6. ★ 닉네임 존재 여부 확인
+    boolean existsByUserNicknameIgnoreCase(String userNickname);
 }
