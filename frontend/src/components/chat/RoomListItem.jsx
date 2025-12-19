682f6ddf7b667458390f2eb637af72cfef7f8cbf
SangHunKim <kim199603330@gmail.com>
2025-12-19 15:38:55 +0900

    12-19 3:38

--- frontend/src/components/chat/RoomListItem.jsx
+++ frontend/src/components/chat/RoomListItem.jsx
@@ -1,5 +1,7 @@
-export default function RoomListItem({ room, active, onClick}){
-    return (
-        
-    )
+export default function RoomListItem({ room, active, onClick }) {
+  return (
+    <div className={`room-item ${active ? "active" : ""}`} onClick={onClick}>
+      <div className="room-name">{room.title}</div>
+    </div>
+  );
 }
--- frontend/src/components/chat/Sidebar.jsx
+++ frontend/src/components/chat/Sidebar.jsx
@@ -1,1 +1,25 @@
 import RoomListItem from "./RoomListItem";
+
+export default function Sidebar({ rooms, activeRoomId, onSelectRoom, me}){
+    return(
+        <aside className="chat-sidebar">
+            <div className="brand">PortFlux</div>
+            <div className="sidebar-title">채팅방 목록</div>
+            
+            <div className="sidebar-section">
+                <div className="section-label">임시채팅그룹</div>
+                {rooms.map((r) => (
+                    <RoomListItem
+                        key={r.roomId}
+                        room={r}
+                        a
+                    />
+                ))}
+
+            </div>
+       
+       
+       
+        </aside>
+    )
+}
